e0f1dc4caf3edb8e41bb97d506cd34757e6a41c4
kaartav <aavulakaartavaraya@gmail.com>
2024-07-04 15:43:26 +0530

    added user.controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,7 +1,70 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiErrors.js";
+import { User } from "../models/model.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "This page is for users to send register request ie POST",
+  //data from front end
+  //validation
+  //not repeted user
+  //check for image and avatar
+  //upload to cloudinary
+  //check if uploaded
+  //create user object- create entry in db
+  //remove password and refersh token from sending response
+  //check for user creation
+  //return conformation(response)
+  const { username, specialization, location, password, email } = req.body;
+  console.log(req.body);
+  if (
+    [username, specialization, location, password, email].some(
+      (field) => field?.trim() === ""
+    )
+  ) {
+    throw new ApiError(400, "All fields are required");
+  }
+
+  const existedUser = User.findOne({ $or: [{ username }, { email }] });
+
+  if (existedUser) {
+    throw new ApiError(409, "Use other username or email");
+  }
+  console.log("\n", existedUser);
+  console.log("\n", req.files);
+  const profilePicLocalPath = req.files?.profilePic[0]?.path;
+  if (!profilePicLocalPath) {
+    profilePicLocalPath = "public/defaultPic.png";
+  }
+
+  const profilePic = await uploadOnCloudinary(profilePicLocalPath);
+  if (!profilePic) {
+    throw new ApiError(409, "There is no profile picture");
+  }
+
+  if (specialization === "") {
+    specialization = "None";
+  }
+  if (location === "") {
+    location = "None";
+  }
+
+  const userrrr = await User.create({
+    username: username.toLowerCase(),
+    email,
+    specialization,
+    location,
+    profile_pic: profilePic.url,
+    password,
   });
+  const createdUser = await User.findById(userrrr._id).select(
+    "-password -refreshToken"
+  );
+  if (!createdUser) {
+    throw new ApiError(500, "something went wrong while uploading to database");
+  }
+  return res
+    .status(201)
+    .json(new ApiResponse(200, createdUser, "User registered succesfully"));
 });
+
 export { registerUser };
--- src/models/model.js
+++ src/models/model.js
@@ -4,80 +4,19 @@ import bcrypt from "bcrypt";
 import dotenv from "dotenv";
 dotenv.config({ path: "./env" });
 
-// Connect to MongoDB
-mongoose.connect("mongodb://localhost:27017/yourDatabaseName", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
 // User Schema
 const UserSchema = new Schema({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-});
-
-const User = mongoose.model("User", UserSchema);
-
-// Profile Schema
-const ProfileSchema = new Schema({
-  _id: { type: Schema.Types.ObjectId, ref: "User" },
-  avatar: String,
+  profile_pic: {
+    type: String, // cloudinary url
+    required: true,
+  },
   specialization: String,
   location: String,
+  refreshToken: String,
 });
-
-const Profile = mongoose.model("Profile", ProfileSchema);
-
-// Blog Schema
-const BlogSchema = new Schema({
-  title: { type: String, required: true },
-  content: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now },
-  author: { type: Schema.Types.ObjectId, ref: "User" },
-  likesCount: { type: Number, default: 0 },
-  commentsCount: { type: Number, default: 0 },
-});
-
-const Blog = mongoose.model("Blog", BlogSchema);
-
-// Media Schema
-const MediaSchema = new Schema({
-  blogId: { type: Schema.Types.ObjectId, ref: "Blog" },
-  url: { type: String, required: true },
-  type: { type: String, required: true },
-});
-
-const Media = mongoose.model("Media", MediaSchema);
-
-// Comment Schema
-const CommentSchema = new Schema({
-  content: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now },
-  blogId: { type: Schema.Types.ObjectId, ref: "Blog" },
-  author: { type: Schema.Types.ObjectId, ref: "User" },
-});
-
-const Comment = mongoose.model("Comment", CommentSchema);
-
-// Reaction Schema
-const ReactionSchema = new Schema({
-  type: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now },
-  blogId: { type: Schema.Types.ObjectId, ref: "Blog" },
-  author: { type: Schema.Types.ObjectId, ref: "User" },
-});
-
-const Reaction = mongoose.model("Reaction", ReactionSchema);
-
-module.exports = {
-  User,
-  Profile,
-  Blog,
-  Media,
-  Comment,
-  Reaction,
-};
 
 //.pre is before and save is just before saving apply this function ..as encription takes a lot of cpu and timr so async /////this is a middle ware
 userSchema.pre("save", async function (next) {
@@ -99,3 +38,45 @@ userSchema.methods.generateRefershTokens = function () {
     expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
   });
 };
+export const User = mongoose.model("User", UserSchema);
+
+// Blog Schema
+const BlogSchema = new Schema({
+  title: { type: String, required: true },
+  content: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+  author: { type: Schema.Types.ObjectId, ref: "User" },
+  likesCount: { type: Number, default: 0 },
+  commentsCount: { type: Number, default: 0 },
+});
+
+export const Blog = mongoose.model("Blog", BlogSchema);
+
+// Media Schema
+const MediaSchema = new Schema({
+  blogId: { type: Schema.Types.ObjectId, ref: "Blog" },
+  url: { type: String, required: true },
+  type: { type: String, required: true },
+});
+
+export const Media = mongoose.model("Media", MediaSchema);
+
+// Comment Schema
+const CommentSchema = new Schema({
+  content: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+  blogId: { type: Schema.Types.ObjectId, ref: "Blog" },
+  author: { type: Schema.Types.ObjectId, ref: "User" },
+});
+
+export const Comment = mongoose.model("Comment", CommentSchema);
+
+// Reaction Schema
+const ReactionSchema = new Schema({
+  type: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+  blogId: { type: Schema.Types.ObjectId, ref: "Blog" },
+  author: { type: Schema.Types.ObjectId, ref: "User" },
+});
+
+export const Reaction = mongoose.model("Reaction", ReactionSchema);
